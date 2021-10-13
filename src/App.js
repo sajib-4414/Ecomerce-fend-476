2ae14e0e8c088d49ea44cf72547023a4b9b074db
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-13 19:30:35 +0600

    removed some comments

--- src/App.js
+++ src/App.js
@@ -7,22 +7,10 @@ function App() {
       <body>
       <NavComp/>
 
-      {/*<div*/}
-      {/*    className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">*/}
-      {/*  <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>*/}
-      {/*  <nav className="my-2 my-md-0 mr-md-3">*/}
-      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Features</a>*/}
-      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Enterprise</a>*/}
-      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Support</a>*/}
-      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Pricing</a>*/}
-      {/*  </nav>*/}
-      {/*  <a className="btn btn-outline-primary" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Sign up</a>*/}
-      {/*</div>*/}
 
       <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 className="display-4">Pricing</h1>
-        <p className="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap
-          example. It's built with default Bootstrap components and utilities with little customization.</p>
+        <h1 className="display-4">Black Friday sale</h1>
+        <p className="lead">The most awaited black friday sale is now on. Browse a range of different products that are not sale</p>
       </div>
 
       <div className="container">
