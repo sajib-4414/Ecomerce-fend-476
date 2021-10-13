f5fcb5ca79233ad1076295d7b55a9e42db33aa9d
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-13 19:28:55 +0600

    added dropdown seller and buyer sign in

--- src/components/NavComp.js
+++ src/components/NavComp.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
+import { Dropdown } from 'react-bootstrap';
 
 class NavComp extends Component{
     render() {
         return(
             <div
                 className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-                <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
+                <h5 className="my-0 mr-md-auto font-weight-normal">Sam-Caleb Marketplace</h5>
                 <nav className="my-2 my-md-0 mr-md-3">
                     <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Features</a>
                     <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Enterprise</a>
                     <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Support</a>
                     <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Pricing</a>
                 </nav>
-                <a className="btn btn-outline-primary" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Sign up</a>
+                <Dropdown>
+                    <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                        Sign in
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">User sign in</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Seller sign in</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </div>
         )
     }
