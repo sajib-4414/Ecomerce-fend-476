cf64118e129381e0837f10cb97f3141c58e092c2
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-13 19:35:44 +0600

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './css/App.css';
 import NavComp from './components/NavComp'
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -61,57 +62,8 @@ function App() {
             </div>
           </div>
         </div>
+      <Footer/>
 
-        <footer className="pt-4 my-md-5 pt-md-5 border-top">
-          <div className="row">
-            <div className="col-12 col-md">
-              {/*<img className="mb-2" src="./images/bootstrap-solid.svg" alt="" width="24"*/}
-              {/*     height="24"/>*/}
-                <small className="d-block mb-3 text-muted">© 2017-2018</small>
-            </div>
-            <div className="col-6 col-md">
-              <h5>Features</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Cool stuff</a>
-                </li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Random
-                  feature</a></li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Team
-                  feature</a></li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Stuff for
-                  developers</a></li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Another
-                  one</a></li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Last time</a>
-                </li>
-              </ul>
-            </div>
-            <div className="col-6 col-md">
-              <h5>Resources</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Resource</a>
-                </li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Resource
-                  name</a></li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Another
-                  resource</a></li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Final
-                  resource</a></li>
-              </ul>
-            </div>
-            <div className="col-6 col-md">
-              <h5>About</h5>
-              <ul className="list-unstyled text-small">
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Team</a></li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Locations</a>
-                </li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Privacy</a>
-                </li>
-                <li><a className="text-muted" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Terms</a></li>
-              </ul>
-            </div>
-          </div>
-        </footer>
       </div>
         
       </body>
