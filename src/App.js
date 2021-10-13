98f2825f4cd2736d7762758ad4ef307beba67c93
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-13 19:04:25 +0600

    created navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import './css/App.css';
+import NavComp from './components/NavComp'
 
 function App() {
   return (
     <div className="App">
       <body>
+      <NavComp/>
 
-      <div
-          className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>
-        <nav className="my-2 my-md-0 mr-md-3">
-          <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Features</a>
-          <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Enterprise</a>
-          <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Support</a>
-          <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Pricing</a>
-        </nav>
-        <a className="btn btn-outline-primary" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Sign up</a>
-      </div>
+      {/*<div*/}
+      {/*    className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">*/}
+      {/*  <h5 className="my-0 mr-md-auto font-weight-normal">Company name</h5>*/}
+      {/*  <nav className="my-2 my-md-0 mr-md-3">*/}
+      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Features</a>*/}
+      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Enterprise</a>*/}
+      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Support</a>*/}
+      {/*    <a className="p-2 text-dark" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Pricing</a>*/}
+      {/*  </nav>*/}
+      {/*  <a className="btn btn-outline-primary" href="https://getbootstrap.com/docs/4.0/examples/pricing/#">Sign up</a>*/}
+      {/*</div>*/}
 
       <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 className="display-4">Pricing</h1>
