a307a9d08cd6a7026672702883b50eb02d0f3205
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-15 17:01:53 +0600

    done designing the company product list page

--- src/components/CompanyProductListToBuyForUserComp.jsx
+++ src/components/CompanyProductListToBuyForUserComp.jsx
@@ -8,17 +8,17 @@ class CompanyProductListToBuyForUserComp extends Component{
                 <div className="col-xl-12">
                     <div className="card">
                         <div className="card-body">
-                            <h5 className="header-title pb-3 mt-0">My previous orders</h5>
+                            <h5 className="header-title pb-3 mt-0">Products from the Company Jane Doe</h5>
                             <div className="table-responsive">
                                 <table className="table table-hover mb-0">
                                     <thead>
                                     <tr className="align-self-center">
                                         <th>Product Name</th>
-                                        <th>Company Name</th>
-                                        <th>Payment Type</th>
-                                        <th>Date Ordered</th>
+                                        <th>Seller Name</th>
+
+                                        <th>In Stock</th>
                                         <th>Item price</th>
-                                        <th>Quantity</th>
+                                        <th>Buy</th>
                                     </tr>
                                     </thead>
                                     <tbody>
@@ -26,55 +26,55 @@ class CompanyProductListToBuyForUserComp extends Component{
                                         <td>Product Devlopment</td>
                                         <td><img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt=""
                                                  className="thumb-sm rounded-circle mr-2"/> Kevin Heal</td>
-                                        <td>Paypal</td>
-                                        <td>5/8/2018</td>
+
+                                        <td><span className='text-success'><b>Yes</b></span></td>
                                         <td>$15,000</td>
-                                        <td><span className="badge badge-boxed badge-soft-warning">panding</span></td>
+                                        <td><button className='btn btn-secondary'>Add to Cart</button></td>
                                     </tr>
                                     <tr>
                                         <td>New Office Building</td>
                                         <td><img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt=""
                                                  className="thumb-sm rounded-circle mr-2"/> Frank M. Lyons</td>
-                                        <td>Paypal</td>
-                                        <td>15/7/2018</td>
+
+                                        <td><span className='text-danger'><b>No</b></span></td>
                                         <td>$35,000</td>
-                                        <td><span className="badge badge-boxed badge-soft-primary">Success</span></td>
+                                        <td><button className='btn btn-secondary'>Add to Cart</button></td>
                                     </tr>
                                     <tr>
                                         <td>Market Research</td>
                                         <td><img src="https://bootdey.com/img/Content/avatar/avatar3.png" alt=""
                                                  className="thumb-sm rounded-circle mr-2"/> Angelo Butler</td>
-                                        <td>Pioneer</td>
-                                        <td>30/9/2018</td>
+
+                                        <td><span className='text-danger'><b>No</b></span></td>
                                         <td>$45,000</td>
-                                        <td><span className="badge badge-boxed badge-soft-warning">Panding</span></td>
+                                        <td><button className='btn btn-secondary'>Add to Cart</button></td>
                                     </tr>
                                     <tr>
                                         <td>Website &amp; Blog</td>
                                         <td><img src="https://bootdey.com/img/Content/avatar/avatar4.png" alt=""
                                                  className="thumb-sm rounded-circle mr-2"/> Phillip Morse</td>
-                                        <td>Paypal</td>
-                                        <td>2/6/2018</td>
+
+                                        <td><span className='text-danger'><b>No</b></span></td>
                                         <td>$70,000</td>
-                                        <td><span className="badge badge-boxed badge-soft-warning">Success</span></td>
+                                        <td><button className='btn btn-secondary'>Add to Cart</button></td>
                                     </tr>
                                     <tr>
                                         <td>Product Devlopment</td>
                                         <td><img src="https://bootdey.com/img/Content/avatar/avatar6.png" alt=""
                                                  className="thumb-sm rounded-circle mr-2"/> Kevin Heal</td>
-                                        <td>Paypal</td>
-                                        <td>5/8/2018</td>
+
+                                        <td><span className='text-danger'><b>No</b></span></td>
                                         <td>$15,000</td>
-                                        <td><span className="badge badge-boxed badge-soft-primary">panding</span></td>
+                                        <td><button className='btn btn-secondary'>Add to Cart</button></td>
                                     </tr>
                                     <tr>
                                         <td>New Office Building</td>
                                         <td><img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt=""
                                                  className="thumb-sm rounded-circle mr-2"/> Frank M. Lyons</td>
-                                        <td>Paypal</td>
-                                        <td>15/7/2018</td>
+
+                                        <td><span className='text-danger'><b>No</b></span></td>
                                         <td>$35,000</td>
-                                        <td><span className="badge badge-boxed badge-soft-primary">Success</span></td>
+                                        <td><button className='btn btn-secondary'>Add to Cart</button></td>
                                     </tr>
                                     </tbody>
                                 </table>
