03868ea07a3cc1a2b7b74a3139915996022dafc7
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-10-27 15:20:34 +0600

    product now adds successfully

--- src/components/AddProductForSeller.jsx
+++ src/components/AddProductForSeller.jsx
@@ -4,6 +4,8 @@ import Select from 'react-select'
 import {Link} from "react-router-dom";
 import jQuery from "jquery";
 import axios from "axios";
+import ProductListForSellerRow from "./ProductListForSeller/ProductListForSellerRow";
+import {toast, Toaster} from "react-hot-toast";
 
 class AddProductForSeller extends Component{
     empty_error_list = {
@@ -41,6 +43,59 @@ class AddProductForSeller extends Component{
         categories:[],
         companies:[]
     }
+    setCompanyAndCategoryInState(companyResponse,categoryResponse){
+        const companies = companyResponse.data
+        const categories = categoryResponse.data
+        const company_array_for_state = []
+        const category_array_for_state = []
+        let is_categories_empty = true
+        let is_companies_empty = true
+
+        if (Object.keys(companies).length !=0){
+            //companies are not empty
+            is_companies_empty = false
+            companies.map(company =>{
+                const item = {label:company.company_name, value:company.pk}
+                company_array_for_state.push(item)
+            })
+        }
+        if (Object.keys(categories).length !=0){
+            //categories are not empty
+            is_categories_empty = false
+            categories.map(category =>{
+                const item = {label:category.name, value:category.pk}
+                category_array_for_state.push(item)
+            })
+        }
+        //because we have to set the state once, multiple times setting state cause problem,
+        //not rendering properly
+        if(!is_categories_empty && !is_companies_empty){
+            this.setState({...this.state,categories:category_array_for_state,companies:company_array_for_state})
+        }
+        else if (!is_categories_empty){
+            this.setState({...this.state,categories:category_array_for_state})
+        }
+        else if (!is_companies_empty){
+            this.setState({...this.state,companies:company_array_for_state})
+        }
+
+    }
+    async componentDidMount() {
+        const [categoryListResponse, companyListResponse] = await Promise.all([
+            axios.get(global.config.bkend.url+"/categories/"),
+            axios.get(global.config.bkend.url+"/companies/")
+        ]);
+        this.setCompanyAndCategoryInState(companyListResponse,categoryListResponse)
+        // this.setState({...this.state,categories:categoryListResponse.data,companies:companyListResponse.data})
+    }
+    handleCategoryChange = (selectedOption) => {
+        this.setState({...this.state,form_data:{...this.state.form_data,category_id:selectedOption.value},errors:{...this.state.errors,category_id:""}})
+        // this.setState({ selectedOptions });
+    }
+    handleCompanyChange = (selectedOption) => {
+        this.setState({...this.state,form_data:{...this.state.form_data,company_id:selectedOption.value}, errors:{...this.state.errors,company_id:""}})
+        // this.setState({ selectedOptions });
+    }
     options = [
         { value: 'address0', label: 'Address0' },
         { value: 'address1', label: 'Address1' },
@@ -73,37 +128,6 @@ class AddProductForSeller extends Component{
                         }})
                 }
             }
-            // else if(target ==="quantity"){
-            //     if(this.validatePassword(val)){
-            //         this.setState({...this.state,[e.target.name]:e.target.value,errors:{
-            //                 ...this.state.errors,[target]:""
-            //             }})
-            //     }
-            //     else{
-            //         this.setState({...this.state,[e.target.name]:e.target.value,errors:{
-            //                 ...this.state.errors,[target]:this.validation_error_list[target]
-            //             }})
-            //     }
-            // }
-            // else if(target ==="confirmPassword") {
-            //     if(this.state.password !==""){
-            //         if(this.state.password !== val){
-            //             this.setState({...this.state,[e.target.name]:e.target.value,errors:{
-            //                     ...this.state.errors,[target]:this.validation_error_list[target]
-            //                 }})
-            //         }
-            //         else{
-            //             this.setState({...this.state,[e.target.name]:e.target.value,errors:{
-            //                     ...this.state.errors,[target]:""
-            //                 }})
-            //         }
-            //     }
-            //     else{
-            //         this.setState({...this.state,[e.target.name]:e.target.value,errors:{
-            //                 ...this.state.errors,[target]:""
-            //             }})
-            //     }
-            // }
             else{
                 //only value is enough for other values
                 this.setState({...this.state,form_data:{...this.state.form_data, [e.target.name]:e.target.value},errors:{
@@ -113,11 +137,24 @@ class AddProductForSeller extends Component{
 
         }
     }
+    getUserIdOrNull(){
+        var retrievedUser = JSON.parse(localStorage.getItem('currentUser'));
+        if(retrievedUser !=null){
+            if ('seller' in retrievedUser){
+                //do
+                const seller = retrievedUser.seller
+                return seller.pk
+            }
+            else{
+                return null
+            }
+        }
+        return null
+    }
     handleFormSubmission(formEvent){
         // alert("I am here submited")
         formEvent.preventDefault()
-        // console.log(this.state)
-        // return;
+
 
 
         //check empty fields and forcefully assign errors
@@ -128,6 +165,8 @@ class AddProductForSeller extends Component{
         for (var key in all_form_data) {
             if (all_form_data.hasOwnProperty(key)) {
                 if (all_form_data[key] === ""){
+                    console.log("printing error value")
+                    console.log("came here because error key is"+key)
                     errors[key] = this.empty_error_list[key]
                 }
 
@@ -146,39 +185,53 @@ class AddProductForSeller extends Component{
         }
         // alert("any error found="+isAnyErrorFound)
         if(isAnyErrorFound){
-            alert("form data something invalid")
+            //alert("form data something invalid")
             return
         }
-        else {
-            this.setState({...this.state,errors:{...this.state.errors,form:""}})
-            alert("form data all valid")
-            // axios
-            //     .post(global.config.bkend.url+"/buyers/", {
-            //         first_name:this.state.first_name,
-            //         last_name:this.state.last_name,
-            //         email:this.state.email,
-            //         username:this.state.username,
-            //         password:this.state.password
-            //     })
-            //     .then(res => {
-            //         window.location.href = '/signupsuccess';
-            //     })
-            //     .catch(errors=>{
-            //         console.log(errors.response)
-            //         const error_response = errors.response
-            //         if(error_response.status === 400){
-            //             this.setState({...this.state,errors:{...this.state.errors,form:error_response.data.email}})
-            //         }
-            //     });
+        this.setState({...this.state,errors:{...this.state.errors,form:""}})
+        //alert("form data all valid")
+            //check if user is logged in
+        const seller_id = this.getUserIdOrNull()
+        if(seller_id ==null){
 
+            this.showToast()
+            return;
         }
+            //so we have a valid seller id
+        axios
+            .post(global.config.bkend.url+"/products/", {
+                name:this.state.form_data.name,
+                price:this.state.form_data.price,
+                quantity:this.state.form_data.quantity,
+                delivery_cost:this.state.form_data.delivery_cost,
+                category_id:this.state.form_data.category_id,
+                company_id:this.state.form_data.company_id,
+                seller_id:seller_id
+            })
+            .then(res => {
+                window.location.href = '/productlistforseller/'+seller_id+'/';
+            })
+            .catch(errors=>{
+                console.log(errors.response)
+                const error_response = errors.response
+                if(error_response.status === 400){
+                    this.setState({...this.state,errors:{...this.state.errors,form:error_response.data.email}})
+                }
+            });
+
+
 
 
 
     }
+    showToast(){
+        toast.error("Please log in as seller first")
+        // toast("Please log in to add products to cart")
+    }
     render() {
         return(
             <div className="width-shrink ">
+                <div><Toaster/></div>
                 <div className="py-5 text-center">
                     <h2>Add Product </h2>
                 </div>
@@ -233,6 +286,7 @@ class AddProductForSeller extends Component{
 
                                 <Select
                                     name="category_id"
+                                    onChange={this.handleCategoryChange.bind(this)}
                                     options={this.state.categories} />
 
                                 <div className="small text-danger">
@@ -247,12 +301,16 @@ class AddProductForSeller extends Component{
 
                                 <Select
                                     name="company_id"
+                                    onChange={this.handleCompanyChange.bind(this)}
                                     options={this.state.companies} />
                                 <div className="small text-danger">
                                     {this.state.errors.company_id}
                                 </div>
                             </div>
-
+                            <div className="text-danger text-center">
+                                {this.state.errors.form}
+                            </div>
+                            <br/>
                             <button className="btn btn-primary btn-lg btn-block" type="submit">Confirm and Add Product
                             </button>
                         </form>
