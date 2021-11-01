29dc5cca219c4841d4f828e208d42ff15a7fba1f
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2021-11-02 02:38:52 +0600

    product search is now working with window.ref from searchbar

--- src/components/navheaderfooter/NavComp.jsx
+++ src/components/navheaderfooter/NavComp.jsx
@@ -36,7 +36,13 @@ class NavComp extends Component{
                 className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
                 <h5 className="my-0 mr-md-auto font-weight-normal">Sam-Caleb Marketplace</h5>
                 {this.props.currentUserType=="seller"?"":
-                    <form className="form-inline my-2 my-lg-0">
+                    <form className="form-inline my-2 my-lg-0"
+                          onSubmit={formEvent=>{
+                              formEvent.preventDefault()
+                              window.location.href = '/productlist?search_query='+this.state.search_text
+                            }
+                          }
+                    >
                         <input
                             value={this.state.search_text}
                             onChange={(event)=>{
